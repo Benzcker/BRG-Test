29cd5f27c423ca22677b98e54a6d168647fb3d0a
Benzcker <benz.checker@googlemail.com>
2018-07-08 22:40:20 +0200

    Alle Fragen in pruefungen.json bewegt

--- pruefungsgenerator.js
+++ pruefungsgenerator.js
@@ -13,21 +13,8 @@ export function extractPruefungen(jsonData={}) {
         }
     }
     return pruefungen;
-
-    // return { novize: { maxMistakes: 2, quests: [new RadioQuest('Wann essen wir endlich?', ['niemals'], ['8:00', '12:00', '18:00']), new FieldQuest('Nenne 3 Tugenden der Ritter.', 3)] } };
 }
 
-        //     'novize': [
-        //         new RadioQuest('Was ist ein dummer Kuchen?', ['Beton'], ['Erdbeere', 'Schokolade']),
-        //         new RadioQuest('Wann essen wir endlich?', ['niemals'], ['8:00', '12:00', '18:00']),
-        //         new FieldQuest('Nenne 3 Tugenden der Ritter.', 3),
-        //         new RadioQuest('Was macht ein echter Ritter?', ['Kämpfen', 'Schminken'], ['Tanzen', 'Singen', 'Lachen']),
-        //         new RadioQuest('Warum werde ich nicht fertig?', ['Nicht genug Bemühung'], ['Internet', 'Ablenkung', 'Schule']),
-        //         new RadioQuest('Welche Farbe hat d. Waffenrock?', ['Blau'], ['Rot', 'Grün', 'Schwarz']),
-        //         new RadioQuest('Wie lange dauert das noch?', ['Für immer'], ['1h', '5h', '14.5 Tage']),
-        //         new RadioQuest('Was ist hier nicht richtig?', ['Ich lerne nicht für Prüfungen'], ['Deine Frisur', 'Deine Antworten']),
-        //     ]
-        // };
 
 function generateQuestion(questionJSON={}) {
     switch (questionJSON['type']) {
